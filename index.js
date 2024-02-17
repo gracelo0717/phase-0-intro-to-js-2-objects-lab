debd0503e8ad3ed5cfc73939a2a1f3fce626ae3a
Grace <gracelo0717@gmail.com>
2024-02-17 18:20:08 -0500

    passed all test

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+const employee = {
+  name: 'Grace',
+  streetAddress: 'Main St'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value,
+  }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  let obj = {
+    ...employee,
+    [key]: undefined,
+  }
+  delete obj.key;
+  return obj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
